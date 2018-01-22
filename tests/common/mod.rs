0ae86a5f5a4f4be9a8d2d2e27ceefb002283728e
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2018-01-22 15:01:26 +0100

    Add first test (oaes)

---
+++ tests/common/mod.rs
@@ -0,0 +1,42 @@
+extern crate oblivc;
+extern crate liback_sys;
+
+use std::thread;
+use std::os::unix::net::UnixStream;
+use std::os::raw::{c_int, c_char};
+use std::ffi::CString;
+use self::oblivc::{ProtocolFn};
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+struct CArgs {
+    pub argc: c_int,
+    pub argv: *mut *mut c_char,
+}
+
+unsafe fn run_party(mut stream: UnixStream, args: Vec<String>, p: c_int, run_fn: ProtocolFn) {
+    // convert Vec<String> to C-style array of pointers
+    let mut ptrs : Vec<_> = args.into_iter().map(
+        |s| CString::new(s).unwrap().into_raw()
+    ).collect();
+    let mut cargs = CArgs {
+        argc: ptrs.len() as c_int,
+        argv: ptrs.as_mut_ptr()
+    };
+    let pd = oblivc::protocol_desc()
+        .party(p)
+        .use_stream(&mut stream);
+    pd.exec_yao_protocol(run_fn, &mut cargs);
+    // reconstruct CStrings for them to be properly freed
+    let _args : Vec<_> = ptrs.into_iter().map(|p| CString::from_raw(p)).collect();
+}
+
+pub unsafe fn run_test(args: Vec<String>, run_fn: ProtocolFn) {
+    let server_args = args.clone();
+    // create a pair of streams corresponding to an anonymous unix socket
+    let (stream1, stream2) = UnixStream::pair().unwrap();
+    // pass one to the server, use the other as client
+    let server = thread::spawn(move || run_party(stream1, server_args, 1, run_fn));
+    run_party(stream2, args, 2, run_fn);
+    server.join().unwrap();
+}
