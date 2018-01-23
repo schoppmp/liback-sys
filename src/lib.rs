12baa8b967a062f0ac47128dd67e26c52f6cddc7
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2018-01-23 12:45:10 +0100

    Add more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-/// # Absentminded Crypto Kit
-///
-/// This library's purpose is to allow adding the [Absentminded Crypto Kit][1] (ACK) as a dependency
-/// to Rust projects using [`oblivc`][2].
-/// Since ACK's functions are written in [Obliv-C][3], this library does not have a public interface.
-/// However, adding it as a dependency will make sure ACK compiles and gets linked to the project.
-///
-/// [1]: https://bitbucket.org/jackdoerner/absentminded-crypto-kit/
-/// [2]: https://schoppmp.github.io/doc/oblivc-rust/oblivc/
-/// [3]: https://github.com/samee/obliv-c
+//! # Absentminded Crypto Kit
+//!
+//! This library's purpose is to allow adding the [Absentminded Crypto Kit][1] (ACK) as a dependency
+//! to Rust projects using [`oblivc`][2].
+//! Since ACK's functions are written in [Obliv-C][3], this library does not have a public interface.
+//! However, adding it as a dependency will make sure ACK compiles and gets linked to the project.
+//!
+//! [1]: https://bitbucket.org/jackdoerner/absentminded-crypto-kit/
+//! [2]: https://schoppmp.github.io/doc/oblivc-rust/oblivc/
+//! [3]: https://github.com/samee/obliv-c
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,7 +2,6 @@ extern crate oblivc;
 extern crate liback_sys;
 
 use std::thread;
-use std::os::unix::net::UnixStream;
 use std::os::raw::{c_int, c_char};
 use std::ffi::CString;
 use self::oblivc::{ProtocolFn};
@@ -14,7 +13,7 @@ struct CArgs {
     pub argv: *mut *mut c_char,
 }
 
-unsafe fn run_party(mut stream: UnixStream, args: Vec<String>, p: c_int, run_fn: ProtocolFn) {
+unsafe fn run_party(args: Vec<String>, p: c_int, run_fn: ProtocolFn) {
     // convert Vec<String> to C-style array of pointers
     let mut ptrs : Vec<_> = args.into_iter().map(
         |s| CString::new(s).unwrap().into_raw()
@@ -23,9 +22,12 @@ unsafe fn run_party(mut stream: UnixStream, args: Vec<String>, p: c_int, run_fn:
         argc: ptrs.len() as c_int,
         argv: ptrs.as_mut_ptr()
     };
-    let pd = oblivc::protocol_desc()
-        .party(p)
-        .use_stream(&mut stream);
+    let pd = oblivc::protocol_desc().party(p);
+    let pd = if p == 1 {
+        pd.accept("34512").expect("Accept failed")
+    } else {
+        pd.connect("localhost", "34512").expect("Connect failed")
+    };
     pd.exec_yao_protocol(run_fn, &mut cargs);
     // reconstruct CStrings for them to be properly freed
     let _args : Vec<_> = ptrs.into_iter().map(|p| CString::from_raw(p)).collect();
@@ -33,10 +35,8 @@ unsafe fn run_party(mut stream: UnixStream, args: Vec<String>, p: c_int, run_fn:
 
 pub unsafe fn run_test(args: Vec<String>, run_fn: ProtocolFn) {
     let server_args = args.clone();
-    // create a pair of streams corresponding to an anonymous unix socket
-    let (stream1, stream2) = UnixStream::pair().unwrap();
     // pass one to the server, use the other as client
-    let server = thread::spawn(move || run_party(stream1, server_args, 1, run_fn));
-    run_party(stream2, args, 2, run_fn);
+    let server = thread::spawn(move || run_party(server_args, 1, run_fn));
+    run_party(args, 2, run_fn);
     server.join().unwrap();
 }
